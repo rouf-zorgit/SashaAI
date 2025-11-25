ac23187fa4c64cee629deedfaee52029baafa03a
Jibon Desktop <rouf@zorgitgroup.com>
2025-11-26 01:50:50 +0600

    Add Sentry to Frontend

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import * as Sentry from "@sentry/react"
 import './index.css'
 import App from './App.tsx'
+
+Sentry.init({
+  dsn: "https://921bf82e45314192081b2dba9fa240a1@o4510425714262016.ingest.us.sentry.io/4510427209203712",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 0.1, // Capture 10% of the transactions
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
